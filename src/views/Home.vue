55de1c37d17b04073738723ccb0648a2576a9b93
George <gchriscarter@gmail.com>
2021-03-30 22:37:03 -0400

    add index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <div v-for="fruit in fruits" v-bind:key="fruit.id">
+      {{ fruit.name }}
+      <hr />
+    </div>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Fruit App!",
+      fruits: [],
+    };
+  },
+  created: function () {
+    this.indexFruits();
+  },
+  methods: {
+    indexFruits: function () {
+      console.log("index");
+      axios.get("http://localhost:3000/api/fruits").then((response) => {
+        console.log(response.data);
+        this.fruits = response.data;
+      });
+    },
   },
 };
 </script>
